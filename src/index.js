496c7e8142565162ef48a42aa50570b7b2319e49
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-01-17 23:08:17 -0500

    class based component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 
-const App = () => {
 
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
-
-    return <div>hi there</div>
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+        return <div>Latitude:</div>
+    }
 }
 
 const container = document.getElementById('root');
